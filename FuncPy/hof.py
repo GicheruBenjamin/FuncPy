df96b73fcf6f76f7ca5ec4acdfc2cb1691f125d7
GicheruBenjamin <projectbuild201@gmail.com>
2024-04-29 15:44:54 +0300

    hofDone

---
+++ FuncPy/hof.py
@@ -0,0 +1,10 @@
+# Higher order functions are those functions that take another func as an input or as an output
+
+def display(f):
+    print(f)
+
+
+def sayHello(name):
+    return f'Hello {name}'
+
+display(sayHello('John'))
